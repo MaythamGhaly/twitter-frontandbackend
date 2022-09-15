7f738356ebee4171e7d59c790c1b4ded01e6f1ca
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-15 11:22:38 +0300

    Clean code and write comments and implement POST method for parameters

--- php/adduser.php
+++ php/adduser.php
@@ -3,94 +3,66 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 include("connection.php");
-$first_name='Mahdi';
-$last_name='Doe';
-$username='@Mahdi';
-$email="mahdi@gmail.com";
-$password='test';
-$profile_picture='';
-$cover_picture='';
-      $image = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAFA3PEY8MlBGQUZaVVBfeMiCeG5uePWvuZHI////////
-////////////////////////////////////////////2wBDAVVaWnhpeOuCguv/////////////
-////////////////////////////////////////////////////////////wAARCAEsASwDASIA
-AhEBAxEB/8QAGQABAQADAQAAAAAAAAAAAAAAAAECAwQF/8QAKhABAQACAQMEAQQBBQAAAAAAAAEC
-EQMhMUEEElFhMhMjM5FxFCJCUoH/xAAXAQEBAQEAAAAAAAAAAAAAAAAAAQID/8QAGhEBAQEBAQEB
-AAAAAAAAAAAAAAERMQISIf/aAAwDAQACEQMRAD8A4gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAWS26gIN2PD/2v/jZMZj2jN9Rcc8wyvaVlOLL6bxPpcaf0cvmJ+jl9N4n1THP
-ePKeGNmu7qLJe82v0Y5Buy4Z/wAf6arLLqtS6ziAKAAAAAAAAAAAAAAAAAAAAAAALJu6gLjjcrqO
-jHCYzp/aYY+2a8+WTna1IAIoLraAAAAAJljMp1UBzZY3G6rF054+6a8+HPZqukus2IAqAAAAAAAA
-AAAAAAAAAAAADbw49fd8NTpwmsJE9X8WMgJ1c2hsw4t9cujPj49db3ZmBJJNSNPJh7budm4s3NVR
-yjPPD236YIAAAADTzY6vunluTOe7GxZcpXKA6MAAAAAAAAAAAAAAAAAAAALjN5SOpz8f5xvY9NRW
-7iw1N3uw4sfdlvxG9FAAFRQTLGZTVac+K49usb1XEcY6ssMcu8YXgnipi60K2/ofbZjhMeyYOUbe
-bHV3GoHNlNZWMWfL/JWDrGAAAAAAAAAAAAAAAAAAAAGfF+cb2jj/ADjpwm8pGPXWo38ePtxjIEUA
-BQAFBUAVRAAY5z3Y2OZ1Ofkx1kzVcvL/ACVrZ8l3nWDpOMUAAAAAAAAAAAAAAAAAAABZdXbt9P1u
-44XZ6L8anpY6qi1GGgAFABQFRQFQRUFHP6nL247dDj9bfxiDlQG2QAAAAAAAAAAAAAAAAAAAB2+j
-/jridvo/wqXix01FRloAQUAFAVFAVBAFHF638o7XD6y/uQnSuYBpkAAAAAAAAAAAAAAAAAAAAdfo
-r0scjp9Hf99iXix2gMqgCKoigKiqgqCgACOD1d/dd7z/AFN3y0nStIDTIAAAAAAAAAAAAAAAAAAA
-A3emuuWfbSz47rPG/ZR6cKmN3IrDaAICooCoKigAAgF7PN5rvlyejndY15mV3lb9rErEBpAAAAAA
-AAAAAAAAAAAAAAAAHpcOXuwlbHm8fLlx3p1nw7eLmxzndizGtbUURUABQAAFQBr5eWceOwTnzmPH
-erzmfJyZcl3f6YNSJQBUAAAAAAAAAAAAAAAAAAAAAAFlsu5eqNnDj7uST46g7sdzFnLKSahZK5tg
-m7O/WLLKAqAKb0xuXx1Nb60Ddy+o0erx/b/x1dDXzz3cdn0qPOAbZAAAAAAAAAAAAAAAAAAAAAAB
-Zjb2lZTjyv0DB2ek49T3Xy0Ti+a7eKy46iVYzAYaEuMqgJ7b8nt+btQCTQugETL8VTLtQeZlNZWf
-FRuz4/dnbL3rC8eX1XRhgMrjZ3lYgAAAAAAAAAAAAAALJb2Z44ecv6bAaZhlfDKcV81sAYzjx/yy
-mMnaQAUQBWWGftrAB2Y5TKbiuTDO410YcuOTFmNazARRYigtrES5Sd6CtXLyeIx5ObfTFp3tqRLV
-EGmVSyXvABLhjfDG8U8VmA1Xjv0lxynhuAc425Yb6zu1AAAAAAAM8MfNYybum0FEAUQBRAFEAUQB
-TaANmPLlj5bJ6j5jnEyLrp/1E+EvqPiOcMhrblz5Vhcre9Yi4iiAKIAogCiAKIArDPHzGQDSLZq6
-QAAAAGeHyyYzsoKIAogCiAKIAogCiAKIAogCiAKIAogCiAKIAogCiAJnPLBsvWNYAACzuiwGQgCi
-AKIAogCiAKIAogCiAKIAogCiAKIAogCiAKIAogCscu6l7AxAAWIAogCiAKIAogCiAKIAogCiAKIA
-ogCiAKIAogCiAKIAogCiAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/Z
-";
-$profile_picture=$image;
-$cover_picture=$image;
 
-$query=$mysqli->prepare("SELECT COUNT(*) as num FROM `users` WHERE email=? LIMIT 1");
-$query->bind_param("s",$email);
-$query->execute();
-$return = $query->get_result()->fetch_assoc();
-// echo $return['num'];
-if($return['num']==1){
-    $response=[];
-    $response["status"]='is_registered';
-    echo json_encode($response);
-}else{
-    date_default_timezone_set('Asia/Beirut');
-    $current_time = date ("Y-m-d");
-    $query=$mysqli->prepare("INSERT INTO `users`(email,first_name,last_name,username,password,created_at) values(?,?,?,?,?,?)");
-    $query->bind_param("ssssss",$email,$first_name,$last_name,$username,$password,$current_time);
+if(isset($_POST['first_name'])){
+    // Get all parameters using POST method
+    $first_name=$_POST['first_name'];
+    $last_name=$_POST['last_name'];
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    // Hash passowrd before insert it into users table
+    $password = hash("sha256", $_POST["password"]);
+    $profile_picture=$_POST['profile_picture'];
+    $cover_picture=$_POST['cover_picture'];
+
+    //Here we have to know if this email is existed before,so, in case it's existed we'll insert it in order to avoid duplicates 
+    $query=$mysqli->prepare("SELECT COUNT(*) as num FROM `users` WHERE email=? LIMIT 1");
+    $query->bind_param("s",$email);
     $query->execute();
-    $id= mysqli_insert_id($mysqli);
-    $profile_picture=base64_decode($profile_picture);
-    $cover_picture=base64_decode($cover_picture);
-    $initial_path="users/".$id;
+    $return = $query->get_result()->fetch_assoc();
 
-    mkdir($initial_path, 755);
-    $profile_picture_path=$initial_path."/profile";
-    $cover_picture_path=$initial_path."/cover";
-    $tweets_path=$initial_path."/tweets";
-
-    mkdir($profile_picture_path, 755);
-    mkdir($cover_picture_path, 755);
-    mkdir($tweets_path, 755);
-    $profile_picture_path=$profile_picture_path."/".strtotime($current_time).".png";
-    $cover_picture_path=$cover_picture_path."/".strtotime($current_time).".png";
-    file_put_contents($profile_picture_path, $profile_picture);
-    file_put_contents($cover_picture_path, $cover_picture);
-
-    $query=$mysqli->prepare("UPDATE `users` SET profile_picture_url=?,cover_picture_url=? WHERE id = ?");
-    $query->bind_param("sss",$profile_picture_path,$cover_picture_path,$id);
-    $query->execute();
-    $response=[];
-    $response["status"]='is_registered';
-    $response["id"]=$id;
-    echo json_encode($response);
-}
-// $array=$query1->get_result();
-// // Create Json response
-// $response=[];
-// while($a = $array->fetch_assoc()){
-//     $response[]= $a;
-//     echo $a['num'];
-// }
-// // Print json response
-// echo json_encode($response);
-?>
+    // If the result is 1, so, this email is exsited before. Hence, we have to return is_registered with JSON.
+    if($return['num']==1){
+        $response=[];
+        $response["status"]='is_registered';
+        echo json_encode($response);
+    }else{
+        // Get the current date and add to attribute created at to be used later on in profile .
+        date_default_timezone_set('Asia/Beirut');
+        $current_time = date ("Y-m-d");
+        // Here we didn't fill profile_picture_url and cover_picture_url since we need to fill them into users folder.
+        $query=$mysqli->prepare("INSERT INTO `users`(email,first_name,last_name,username,password,created_at) values(?,?,?,?,?,?)");
+        $query->bind_param("ssssss",$email,$first_name,$last_name,$username,$password,$current_time);
+        $query->execute();
+        // Here we want to get the last id inserted into users table
+        $id= mysqli_insert_id($mysqli);
+        //Decode pictures  
+        $profile_picture=base64_decode($profile_picture);
+        $cover_picture=base64_decode($cover_picture);
+        // Since users folder is created by default, so, we need to create folder called same as id
+        $initial_path="users/".$id;
+        mkdir($initial_path, 755);
+        // Start creating profile folder,cover folder and tweets that will contain the tweets of this user
+        $profile_picture_path=$initial_path."/profile";
+        $cover_picture_path=$initial_path."/cover";
+        $tweets_path=$initial_path."/tweets";
+        mkdir($profile_picture_path, 755);
+        mkdir($cover_picture_path, 755);
+        mkdir($tweets_path, 755);
+        // Rename both profile and cover picture and fill them into their folders respectively
+        $profile_picture_path=$profile_picture_path."/".strtotime($current_time).".png";
+        $cover_picture_path=$cover_picture_path."/".strtotime($current_time).".png";
+        file_put_contents($profile_picture_path, $profile_picture);
+        file_put_contents($cover_picture_path, $cover_picture);
+        //Now, we have the path. Hence, we have to update profile_picture_url and  cover_picture_url into users table
+        $query=$mysqli->prepare("UPDATE `users` SET profile_picture_url=?,cover_picture_url=? WHERE id = ?");
+        $query->bind_param("sss",$profile_picture_path,$cover_picture_path,$id);
+        $query->execute();
+        // Once these steps done, we have to return a JSON response containing status and id
+        $response=[];
+        $response["status"]='done';
+        $response["id"]=$id;
+        echo json_encode($response);
+    }//End of else
+}//End of isset method
+    ?>
