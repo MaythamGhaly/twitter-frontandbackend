30b5ef939d3ea94e9ac3e6b00cf89ae63e2b1075
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-15 23:44:22 +0300

    Review and clean code and write some comments

--- php/visitprofile.php
+++ php/visitprofile.php
@@ -11,35 +11,49 @@ if(isset($_GET['id'])){
     $query=$mysqli->prepare("SELECT first_name,last_name,username,profile_picture_url,cover_picture_url,created_at FROM users WHERE id=? LIMIT 1");
     $query->bind_param("s",$id);
     $query->execute();
-    $array=$query->get_result()->fetch_assoc();
-    // Create JSON response and return profile data of this user
-    $response=[];
-    $response["first_name"]= $array['first_name'];
-    $response["last_name"]= $array['last_name'];
-    $response["profile_picture_url"]= $array['profile_picture_url'];
-    $response["cover_picture_url"]= $array['cover_picture_url'];
-    $response["created_at"]= $array['created_at'];
+    $array_profile=$query->get_result()->fetch_assoc();
 
-    // Now, we have to add to JSON response his tweets, their numbers and their pictures.
-    $response_tweets1=[];
+    // When we want to create JSON response through this page, it will be divided into 2 main info: 
+    // 1-General data like first name, last name...
+    // 2-All the tweets of this user which is divided into many info:
+        // 2-1) General info like text, date of this tweet...
+        // 2-2)Every single picture for this tweet wich contains only the url of the picture
+    // Create JSON response which will be returned later on
+    $response=[];
+    $response["first_name"]= $array_profile['first_name'];
+    $response["last_name"]= $array_profile['last_name'];
+    $response["profile_picture_url"]= $array_profile['profile_picture_url'];
+    $response["cover_picture_url"]= $array_profile['cover_picture_url'];
+    $response["created_at"]= $array_profile['created_at'];
+
+    // Now, we have to add to JSON response user's tweets, their numbers and their pictures.
+    // response_tweets_data is the array containing all the data about each tweet
+    $response_tweets_data=[];
+    // response_tweets is the array containing all the data about all tweets
     $response_tweets=[];
+    // Now, we are getting data of the tweets in addition to the number of likes of each form another table which is tweets_likes.
     $query=$mysqli->prepare("SELECT tweets.id,tweets.text,tweets.created_at,COUNT(tweets.id) as likes FROM tweets,tweets_likes WHERE tweets.user_id=? AND tweets.id=tweets_likes.tweets_id GROUP BY tweets.id");
     $query->bind_param("s",$id);
     $query->execute();
-    $array=$query->get_result();
-    while($a = $array->fetch_assoc()){
-        // $response_tweets['id'][]= $a['id'];
-        // $response_tweets['text'][]= $a['text'];
-        // $response_tweets['created_at'][]= $a['created_at'];
-        // // $response_tweets[]=$response_tweets;
-        $response_tweets1['id']=$a['id'];
-        $response_tweets1['text']=$a['text'];
-        $response_tweets1['created_at']=$a['created_at'];
-        $response_tweets1['likes']=$a['likes'];
-        $response_tweets[]=$response_tweets1;
+    $array_tweets=$query->get_result();
+    while($a = $array_tweets->fetch_assoc()){
+        $response_tweets_data['id']=$a['id'];
+        $response_tweets_data['text']=$a['text'];
+        $response_tweets_data['created_at']=$a['created_at'];
+        $response_tweets_data['likes']=$a['likes'];
+        // Now, we want to get the pictures' urls of every tweet and put them inside response_tweets_data
+        $query=$mysqli->prepare("SELECT picture_url FROM tweets_pictures WHERE tweets_id=?");
+        $query->bind_param("s",$a['id']);
+        $query->execute();
+        $array_pictures=$query->get_result();
+        // picture_url is the array that will contains all pictures' urls of the tweet
+        $picture_url=[];
+        while($b=$array_pictures->fetch_assoc()){
+               $picture_url[]=$b['picture_url'];
+        }
+        $response_tweets_data['picture_urls']=$picture_url;
+        $response_tweets[]=$response_tweets_data;
     }
-    // echo json_encode($response_tweets);
-    // echo '<br>';
     $response['tweets']=$response_tweets;
     echo json_encode($response);
 }
