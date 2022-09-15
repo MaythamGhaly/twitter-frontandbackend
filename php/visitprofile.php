e928762e61b75780a72817c5afaa7034bffa4974
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-15 12:25:54 +0300

    Test visit profile using postman and update some comments

--- php/visitprofile.php
+++ php/visitprofile.php
@@ -7,12 +7,12 @@ if(isset($_GET['id'])){
     // Get id using GET method
     $id=$_GET['id'];
 
-    // We don't need all data, we only need data that everyone can see.
+    // We don't need all data, we only need data that everyone can see. hence, we shouldn't return sensitive data like passowrd,email ...
     $query=$mysqli->prepare("SELECT first_name,last_name,username,profile_picture_url,cover_picture_url,created_at FROM users WHERE id=? LIMIT 1");
     $query->bind_param("s",$id);
     $query->execute();
     $array=$query->get_result()->fetch_assoc();
-    // Create Json response and return it
+    // Create JSON response and return it
     $response=[];
     $response[]= $array;
     echo json_encode($response);
