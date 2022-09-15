3d1085ed5a4e8545a4b5f13bb74de7d96f8e21ed
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-15 08:44:40 +0300

    Create connection as php file and test it

---
+++ php/connection.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+$host="localhost";
+$db_user="root";
+$db_pass=null;
+$db_name="twitterdb";
+
+if($mysqli =new mysqli($host,$db_user,$db_pass,$db_name)){
+    echo 'done';
+}
+else{
+    echo 'fail';    
+}
+?>
