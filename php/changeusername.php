e8c4c3ccc77108200e3c96e7dd2f9e491195374a
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-16 01:10:42 +0300

    Create JSON response, add comments and test page with postman

--- php/changeusername.php
+++ php/changeusername.php
@@ -3,17 +3,18 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 include("connection.php");
-// if(isset($_POST['id'])){
-
-    // $username=$_POST['username'];
-    // $id=$_POST['id'];
-    $username = "@houssein1231";
-    $id="1";
-    $query=$mysqli->prepare("UPDATE `users` SET`users`.username=? WHERE id=?");
+if(isset($_POST['id'])){
+// Get all parameters using POST method
+    $username=$_POST['username'];
+    $id=$_POST['id'];
+    $query=$mysqli->prepare("UPDATE `users` SET `users`.username=? WHERE id=?");
     $query->bind_param("ss",$username,$id);
+    $response=[];
+    // In case the query is executed, we have to return done, otherwise, we have to return error
     if($query->execute()){
-        echo "done";
+        $response["status"]='done';
     }else{
-        echo "fail";
+        $response["status"]='error';
     }
-// }
+    echo json_encode($response);
+}
