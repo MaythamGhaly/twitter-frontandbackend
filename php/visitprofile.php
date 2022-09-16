c45443ef4386a770dc26dabf9785e741ed47d9b3
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-16 11:30:14 +0300

    Add the relation between while visitng a profile between users

--- php/visitprofile.php
+++ php/visitprofile.php
@@ -7,10 +7,11 @@ include("connection.php");
 if(isset($_GET['id'])){
     // Get id using GET method
     $id=$_GET['id'];
+    $other_id=$_GET['other_id'];
 
     // We don't need all data, we only need data that everyone can see. hence, we shouldn't return sensitive data like passowrd,email ...
     $query=$mysqli->prepare("SELECT `users`.first_name,`users`.last_name,`users`.username,`users`.profile_picture_url,`users`.cover_picture_url,`users`.created_at, 
-    COUNT( CASE WHEN `followers`.users_id = ? THEN 1 END ) AS following,
+    COUNT( CASE WHEN `followers`.user_id = ? THEN 1 END ) AS following,
     COUNT( CASE WHEN `followers`.user_following = ? THEN 1 END ) AS followers 
     FROM users,followers
     WHERE `users`.`id`=? LIMIT 1");
@@ -38,10 +39,11 @@ if(isset($_GET['id'])){
     // response_tweets is the array containing all the data about all tweets
     $response_tweets=[];
     // Now, we are getting data of the tweets in addition to the number of likes of each form another table which is tweets_likes.
-    $query=$mysqli->prepare("SELECT tweets.id,tweets.text,tweets.created_at,COUNT(tweets.id) as likes ,COUNT( CASE WHEN tweets_likes.users_id = ? THEN 1 END ) AS liked FROM tweets,tweets_likes WHERE tweets.user_id=? AND tweets.id=tweets_likes.tweets_id GROUP BY tweets.id ORDER BY tweets.created_at DESC");
+    $query=$mysqli->prepare("SELECT tweets.id,tweets.text,tweets.created_at,COUNT(tweets.id) as likes ,COUNT( CASE WHEN tweets_likes.user_id = ? THEN 1 END ) AS liked FROM tweets,tweets_likes WHERE tweets.user_id=? AND tweets.id=tweets_likes.tweets_id GROUP BY tweets.id ORDER BY tweets.created_at DESC");
     $query->bind_param("ss",$id,$id);
     $query->execute();
     $array_tweets=$query->get_result();
+
     while($a = $array_tweets->fetch_assoc()){
         $response_tweets_data['id']=$a['id'];
         $response_tweets_data['text']=$a['text'];
@@ -65,5 +67,38 @@ if(isset($_GET['id'])){
     }
     // Put the prepared data into the JSON response (same as first name, last name...)
     $response['tweets']=$response_tweets;
+    // After the above operations, we need to get the relation between id and other id.
+    // Hence, if user is visiting his profile, so, the relation will be nothing.
+    // Another case, if user is visiting another profile, here we have to show 4 cases:
+        // 1-if first user is following the other user, so, the response must be 'following'.
+        // 2-if first user is followed by the other user, so, the response must be 'follows you'.
+        // 3-if both first user and other user follow each other, so, the response must be 'friends'.
+        // 4-if neither first user is following the other user nor vice versa, so, the response must be 'not friends'.
+    $query = $mysqli->prepare('SELECT * from followers WHERE (followers.user_id=? or followers.user_id=?) and(followers.user_following=? or followers.user_following=?)');    
+    $query->bind_param("ssss",$id,$other_id,$id,$other_id);
+    $query->execute();
+    $return=$query->get_result();
+    $row= mysqli_num_rows($return);
+
+    // If first id is equal to other id that means the user is visiting his profile
+    if($id==$other_id){
+        $response['relation']='same profile';
+    // if the return number is 0 that means they don't follow each other
+    }else if($row==0){
+        $response['relation']='not firends';
+    // if the return number is 1 that means the first id user following the second user or vice versa
+    }else if($row==1){
+        $return=$return->fetch_assoc();
+        // Here, the first user is following second user
+        if($return['user_id']==$id){
+            $response['relation']='following';
+        // Here, the first user is following second user
+        }else{
+            $response['relation']='follows you';
+        }
+    // if the return number is 2 that means they are following each other
+    }else{
+        $response['relation']='friends';
+    }
     echo json_encode($response);
 }
