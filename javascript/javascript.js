ded69f75780e981087174c9b7f824ef531edbc80
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-17 02:02:27 +0300

    add popup-signup

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,22 +1,91 @@
 window.onload = () => {
-    const btn=document.getElementById("log-in-btn")
-    const signin=document.getElementById("login-page")
-    const popup=document.getElementById("pop-up")
+    const btn_signin = document.getElementById("log-in-btn")
+    const btn_signup = document.getElementById("signup-btn")
+    const signin = document.getElementById("login-page")
+    const pop_signin = document.getElementById("pop-signin")
+    const pop_signup=document.getElementById("pop-signup")
     const x = window.matchMedia("(max-width: 786px)")
-    console.log("no")
-    // if (x.matches){
-    //     signin.style.display="none"
-    // }
-    btn.addEventListener("click",()=>{
-        console.log("yes")
-        popup.style.display="block"
-        
-        
-    })
+    const close = document.getElementById("close")
+    const close_signup = document.getElementById("close-signup")
+    let btnstatus=false
+
+    if (x.matches ) {
+        btn_signin.addEventListener("click", () => {
+            btnstatus=true
+            pop_signin.style.display = "block"
+            signin.style.display = "none"
+        })
+        close.addEventListener("click", () => {
+            btnstatus=false
+            pop_signin.style.display = "none"
+            signin.style.display = "block"
+        })
+    }
+    else {
+        btn_signin.addEventListener("click", () => {
+            btnstatus=true
+            pop_signin.style.display = "block"
+        })
+        close.addEventListener("click", () => {
+            btnstatus=false
+            signin.style.display = "block"
+            pop_signin.style.display = "none"
+        })
+    }
+    window.addEventListener('resize', () => {
+        if (btnstatus && x.matches) {
+            signin.style.display = "none"
+            close.addEventListener("click", () => {
+                pop_signin.style.display = "none"
+            })
+            close.addEventListener("click", () => {
+                pop_signin.style.display = "none"
+            })
+        }
+    });
+
+    if (x.matches ) {
+        btn_signup.addEventListener("click", () => {
+            btnstatus=true
+            pop_signup.style.display = "block"
+            signin.style.display = "none"
+        })
+        close_signup.addEventListener("click", () => {
+            btnstatus=false
+            pop_signup.style.display = "none"
+            signin.style.display = "block"
+        })
+    }
+    else {
+        btn_signup.addEventListener("click", () => {
+            btnstatus=true
+            pop_signup.style.display = "block"
+        })
+        close_signup.addEventListener("click", () => {
+            btnstatus=false
+            signin.style.display = "block"
+            pop_signup.style.display = "none"
+        })
+    }
+    window.addEventListener('resize', () => {
+        if (btnstatus && x.matches) {
+            signup.style.display = "none"
+            close_signup.addEventListener("click", () => {
+                pop_signup.style.display = "none"
+            })
+            close_signup.addEventListener("click", () => {
+                pop_signup.style.display = "none"
+            })
+        }
+    });
+    
+
+
+}
+    
 
     
 
 
 
 
-}
