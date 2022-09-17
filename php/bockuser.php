a4c060e6e23d67ddb4366f9b1133d01e6072c769
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-17 08:14:16 +0300

    Finish block user page

---
+++ php/bockuser.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+include("connection.php");
+
+if(isset($_POST['id'])){
+    $id=$_POST['id'];
+    $other_id=$_POST['other_id'];
+    $query= $mysqli->prepare("DELETE *  FROM `followers` WHERE (followers.user_id=? and followers.user_following=?) OR (followers.user_id=? and followers.user_following=?)");
+    $query->bind_param("ssss",$id,$other_id,$other_id,$id);
+    if($query->execute()){
+        $query=$mysqli->prepare("INSERT INTO blockes(user_id,user_blocking) values(?,?)");
+        $query->bind_param("ss",$id,$other_id);
+        if($query->execute()){
+            $response=[];
+            $response['status']="done";
+            echo json_encode($response);
+        }
+    }
+}
+?>
