a72cc28b3d8d59ba9afd99df11a65634b8550583
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-17 23:16:13 +0300

    add edit popup

--- Profile page/javascript/javascript.js
+++ Profile page/javascript/javascript.js
@@ -1,8 +1,19 @@
 window.onload = () => {
+    const edit=document.getElementById("edit")
+    const profile=document.getElementById("container")
+    const popup=document.getElementById("edit-popup")
+    const x = window.matchMedia("(max-width: 786px)")
+    const close=document.getElementById("close")
 
+    edit.addEventListener("click", () => {
+                btnstatus=true
+                popup.style.display = "block"
+                profile.style.display = "none"
+            })
+            close.addEventListener("click", () => {
+                btnstatus=false
+                popup.style.display = "none"
+                profile.style.display = "block"
+            })
 
-
-
-
-    
 }
