18800f46ceec51508b378598ef545dafd3eaac39
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-17 15:57:26 +0300

    Clean code and put some comments

--- php/tweet.php
+++ php/tweet.php
@@ -5,14 +5,18 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorizatio
 include("connection.php");
 
 if(isset($_POST['id'])){
-
+    //Get all parameters using POST method 
     $id=$_POST['id'];
     $text=$_POST['text'];
     date_default_timezone_set('Asia/Beirut');
     $current_time = date ("Y-m-d");
+    // The below query will add the text to tweets table into DataBase
     $query=$mysqli->prepare("INSERT INTO tweets(text,created_at,user_id) VALUES(?,?,?)");
     $query->bind_param("sss",$text,$current_time,$id);
+    
     $response=[];
+    // In case the above query has been executed, the JSON response must contain the id of last tweet uploaded.
+    // Otherwise, an error will be returned with JSON response also
     if($query->execute()){
         $tweet_id= mysqli_insert_id($mysqli);
         $response['status']="done";
