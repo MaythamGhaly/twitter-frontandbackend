b9cb51176afd744a195fbab14953754e6dd64bbd
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-18 13:22:21 +0300

    Clean code and write some comments

--- twitter-frontandbackend-mathambranch/others profile page/scripts.js
+++ twitter-frontandbackend-mathambranch/others profile page/scripts.js
@@ -8,23 +8,38 @@ const username=document.getElementById("username");
 const join_date=document.getElementById("join_date");
 const following_number=document.getElementById("following_number");
 const follwer_number=document.getElementById("follwer_number");
-const folow_button= document.getElementById("folow_button");
+const follow_button= document.getElementById("follow_button");
 const block_button=document.getElementById("block_button");
 
+let relation="";
 // The below url will be used for the pcitures of every tweet on top left
-let profile_url="./images/PP.png";
+let profile_url="../Profile page/images/PP.png";
 // Fetch all data of the user in one single JSON response
-localStorage.setItem("id",19);
-localStorage.setItem("other_id",2);
 let url = `http://localhost/twitter-frontandbackend/php/visitprofile.php?id=${localStorage.getItem("id")}&other_id=${localStorage.getItem("other_id")}`;
      
     fetch(url)
     .then(respone=>respone.json())
     .then(data=>{
-
+        // relation variable is the relation between first user and other one. Hence we've 4 cases:
+        // 1-If thery aren't friends the button's innerText must be follow
+        // 2-If 1st user follows 2nd user, the button's innerText must be following
+        // 3-If 1st user is followed by 2nd user, the button's innerText must be follow back
+        // 4-If thery are following each other, the button's innerText must be friends
+        relation=Object.values(data)[9];
+            if(relation=="not firends"){
+                follow_button.innerText="Follow";
+            }else if(relation=="following"){
+                follow_button.innerText="Following";
+            }else if(relation=="follows you"){
+                follow_button.innerText="follow back";
+            }else if(relation=="friends"){
+                follow_button.innerText="Friends";
+            }
+               
     name1.innerText=Object.values(data)[0];
-if((Object.values(data)[3])!="NA"){
+    if((Object.values(data)[3])!="NA"){
     // Get cover photo
+    // Put try catch exception to avoid them
     try{cover_photo.src="http://localhost/twitter-frontandbackend/php/".concat(Object.values(data)[3]);}
     catch(err){}
 }
@@ -42,16 +57,17 @@ username.innerText=`@${Object.values(data)[4]}`;
 let toMonthName=(monthNumber)=> {
     const date = new Date(Object.values(data)[5]);
     date.setMonth(monthNumber - 1);
-    
     return date.toLocaleString('en-LB', {
         month: 'long',
     });
     }
+// The result of the day will be like : Joined 3, September 2022
 const date= Object.values(data)[5];
 const array=date.split("-");
 join_date.innerText=`Joined ${array[2]},${toMonthName(array[1])} ${array[0]}`;
 following_number.innerText=`${Object.values(data)[6]} Following`
 follwer_number.innerText=`${Object.values(data)[7]} Followers`
+
 if(Object.values(data)[8].length<=1){
     tweets_number.innerText=`${Object.values(data)[8].length} Tweet`
 }else{
@@ -59,7 +75,7 @@ if(Object.values(data)[8].length<=1){
 }
 let array_tweets=Object.values(data)[8];
     
-// Like tweet, so when the response is liked, the color will be red.
+// Like tweet, so when the response is as 'liked', the color will be red.
 let likeTweet=(id,tweet_id,heart)=>{
         let url = "http://localhost/twitter-frontandbackend/php/liketweet.php";
         let parameters = {
@@ -77,7 +93,7 @@ let likeTweet=(id,tweet_id,heart)=>{
             heart.style.backgroundColor='white';
 ;                   }
         });}
-// Like tweet, so when the response is unliked, the color will be gray.
+// Like tweet, so when the response is as unliked, the color will be gray.
 let unLikeTweet=(id,tweet_id,heart)=>{
         let url = "http://localhost/twitter-frontandbackend/php/unliketweet.php";
         let parameters = {
@@ -133,8 +149,9 @@ for(let i=0;i<array_tweets.length;i++){
 for(let j=0;j<array_tweet_pictures.length;j++){
     const tweet_img=document.createElement('img');
     tweet_img.classList.add('tweet-img');
+    if(array_tweet_pictures[j]!="NA"){
     tweet_img.src="http://localhost/twitter-frontandbackend/php/".concat(array_tweet_pictures[j]);
-    images.appendChild(tweet_img);
+    images.appendChild(tweet_img);}
 }
     tweet_text.appendChild(p1);
     tweet_text.appendChild(images);
@@ -184,10 +201,7 @@ for(let j=0;j<array_tweet_pictures.length;j++){
 }
 });
 
-
-// {/* <button class="edit-profile">Follow <span>&#43;</span></button> */}
-// {/* <button class="Following">Following <span>&#10004;</span></button> */}
-
+// Send block request to the server in case 1st user clicked on block button
 let blockUser = ()=>{
         let url = "http://localhost/twitter-frontandbackend/php/blockuser.php";
         let parameters = {
@@ -207,4 +221,53 @@ let blockUser = ()=>{
             }
         });
 }
+// In order to avoid orginizing many buttons, and to make the follow idea easier, here we've 4 cases:
+// 1- If the innerText of follow button was follow or follow back which mean that 1st user didn't follow 2nd user yet.Hence:
+    // a)if 1st user is followed by 2nd user, the follow button's innerText will be follow back and after follow request, the result will be friends
+    // b)if 1st and 2nd user aren't friends, the folllow button's innerText will be follow and after follow request, the result will be following sice 2nd user didn't follow 1st user yet.
+// 2)If the innerText of follow button was following, when 1st user re-clink on it, a 'unfollow' request will be sent to the server and the result will be follow, since they aren't firends anymore unless someone follow the other one.
+
+let followUser = ()=>{
+    if(follow_button.innerText=="Follow" || follow_button.innerText=="follow back"){
+        let url = "http://localhost/twitter-frontandbackend/php/followuser.php";
+        let parameters = {
+            method:'POST',
+            body: new URLSearchParams({
+                id:localStorage.getItem("id"),
+                other_id:localStorage.getItem("other_id")
+            })
+        };
+        fetch(url,parameters)
+        .then(respone=>respone.json())
+        .then(data=>{
+            console.log(data);
+            if(Object.values(data)[0]=="done"){
+                if(follow_button.innerText=="Follow"){
+                    follow_button.innerText="Following";}
+                else{
+                    follow_button.innerText="Friends";
+                }
+            }
+        });
+    }else if(follow_button.innerText=="Following"){
+        let url = "http://localhost/twitter-frontandbackend/php/unfollowuser.php";
+        let parameters = {
+            method:'POST',
+            body: new URLSearchParams({
+                id:localStorage.getItem("id"),
+                other_id:localStorage.getItem("other_id")
+            })
+        };
+        fetch(url,parameters)
+        .then(respone=>respone.json())
+        .then(data=>{
+            console.log(data);
+            if(Object.values(data)[0]=="done"){
+                follow_button.innerText="Follow";
+            }
+        });
+    }
+
+}
 block_button.addEventListener('click',blockUser);
+follow_button.addEventListener('click',followUser);
